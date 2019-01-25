24eefb6c1c69ee6cce064dad011f7069009691df
FredSt0ne <fredstone624@gmail.com>
2019-01-25 20:36:34 +0300

    edit about section and minify images

--- Home-Page/js/layout.js
+++ Home-Page/js/layout.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
-    $('body').scrollspy({ target: '#navbar' });
+    navActiveItem();
+    scrollToLink();
+    scrollToTop();
 });
+
+function navActiveItem() {
+    $('body').scrollspy({ target: '#navbar' });
+}
+
+function scrollToLink(){
+    $('.js_link').on("click", function(e){
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 777);
+
+        e.preventDefault();
+        return false;
+    });
+}
+
+function scrollToTop(){
+    $('body').materialScrollTop();
+}
