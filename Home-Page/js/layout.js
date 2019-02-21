c58297cd58ed56273df70d91bf960022a78526a3
FredSt0ne <fredstone624@gmail.com>
2019-02-21 03:59:09 +0300

    changed header section and script

--- Home-Page/js/layout.js
+++ Home-Page/js/layout.js
@@ -1,10 +1,6 @@
 $(document).ready(function(){
-
     hamburgerMenuInit();
-    navActiveInit();
     backToTopInit();
-    animationInit();
-    // galleryInit();
     parallaxInit();
 });
 
@@ -14,28 +10,8 @@ function hamburgerMenuInit(){
     });
 }
 
-function navActiveInit() {
-    $('body').scrollspy({ target: '#navbar' });
-}
-
 function backToTopInit(){
     $('body').materialScrollTop();
-}
-
-function animationInit(){
-    wow = new WOW({
-        boxClass: 'wow',
-        animateClass: 'animated',
-        offset: 0,
-        mobile: false,
-        live: true
-    })
-
-    wow.init();
-}
-
-function galleryInit(){
-    $("#mdb-lightbox-ui").load("../common/mdb-addons/mdb-lightbox-ui.html");
 }
 
 function parallaxInit(){
